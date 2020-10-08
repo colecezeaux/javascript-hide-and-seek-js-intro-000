0cdc33d31b4a34a03400c994a904456baf1cb7e3
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-08 20:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// Define a function getFirstSelector(selector), which accepts a selector and returns the first element that matches.
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector)
+  return element
+}
 
+// Define a function nestedTarget() that pulls a .target out of #nested (# is used for IDs in selectors).
+// (Note that in index.html #nested and .target just happen to be divs. This method should work with arbitrary elements.)
+function nestedTarget(Id) {
+  var className = document.querySelector(Id)
+  return className
+}
+
+// Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()
+function deepestChild() {
+
+}
+
+// Define a function deepestChild() that pulls out the most deeply nested child element from div#grand-node.
+// (Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them.)
+function increaseRankBy(n) {
+
+}
